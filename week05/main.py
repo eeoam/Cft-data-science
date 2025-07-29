cc6587239c62405a3ef92a9848217483ef9a6aa9
Eric Macaulay <emmanuel@whetztone.net>
2025-07-29 23:16:59 +0100

    Week 5 - Add section "Creating a list"

---
+++ week05/main.py
@@ -0,0 +1,12 @@
+def main():
+    print("Hello from week05!")
+
+
+if __name__ == "__main__":
+    main()
+
+# uv init week05
+# uv venv
+# source .venv/bin/activate
+# vscode>create new jupyter notebook
+# vscode: select kernel > python environment > 
